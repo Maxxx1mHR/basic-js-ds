b12eba332b3701319cd7dcaf298dc758c3980ad9
Your Name <you@example.com>
2023-04-13 23:01:43 +0300

    feat: add binary-search-tree add()

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -6,6 +6,17 @@ const { Node } = require('../extensions/list-tree.js');
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
+// class Node
+// {
+//     constructor(data)
+//     {
+//         this.data = data;
+//         this.left = null;
+//         this.right = null;
+//     }
+// }
+
 class BinarySearchTree {
 
   constructor() {
@@ -18,19 +29,93 @@ class BinarySearchTree {
     return this.head;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+
+  add(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    }
+    let cur = this.head;
+    while (cur) {
+      if (newNode.data > cur.data) {
+        if (cur.right === null) {
+          cur.right = newNode;
+        }
+      } else {
+        if (cur.left === null) {
+          cur.left = newNode;
+        }
+      }
+      return cur
+    }
+
+
+    // while (cur) {
+    //   if (newNode.data < cur.data) {
+    //     if(cur.left === null) {
+    //       cur.left === newNode;
+    //   } else {
+
+    //   }
+    // }
+
+    // return this.head;
+
+
+    // while (cur) {
+    //   if (newNode.data > cur.data) {
+    //     if (cur.right === null) {
+    //       cur.right = newNode;
+    //       return;
+    //     }
+    //     cur = cur.right;
+    //   } else {
+    //     if (cur.left === null) {
+    //       cur.left = newNode;
+    //       return;
+    //     }
+    //     cur = cur.left;
+    //   }
+    // }
+
+    // return cur;
+
+
+
   }
+
+
+
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(this.head === null) {
+      return null;
+    }
+
+    let cur = this.head;
+
+    while(cur) {
+      if(data > cur.data) {
+        cur = cur.right;
+      } else if (data < cur.data) {
+        cur = cur.left;
+      } else {
+      }
+      return cur;
+    }
+
+
+
   }
 
   remove(/* data */) {
