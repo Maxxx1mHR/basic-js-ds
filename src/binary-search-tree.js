90fb18490f1b4b1b2606558ac2c9583b0137e551
Maxim <maksim.pudeev@bk.ru>
2023-04-13 16:21:29 +0300

    feat: start doing binary-search-tree. add return root element

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,9 +8,14 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  constructor() {
+    this.head = null;
+}
+
   root() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.head;
   }
 
   add(/* data */) {
@@ -47,3 +52,18 @@ class BinarySearchTree {
 module.exports = {
   BinarySearchTree
 };
+
+
+// class Node {
+//   constructor(data) {
+//       this.data = data; // node value
+//       this.left = null;   // left node child reference
+//       this.right = null; // right node child reference
+//   }
+// }
+
+// class BinarySearchTree {
+//   constructor() {
+//       this.root = null; // корень bst
+//   }
+// }
